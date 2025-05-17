c723a116f33a90dd794bc0648e00355c967024af
Veronica22-stack <verosilvaa2206@gmail.com>
2025-05-17 09:15:03 -0300

    Delete antlr-seguncaso-project directory

--- antlr-seguncaso-project/CustomseguncasoListener.js
+++
@@ -1,8 +0,0 @@
-import {seguncasoListener} from "./generated/seguncasoListener.js";  
-
-export class CustomseguncasoListener extends seguncasoListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-}
